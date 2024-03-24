296106ad4415d459dc7c87746dba36adf2a15ab8
bgoncharuck <dariydragon26@gmail.com>
2024-03-24 10:35:53 +0200

    feat: resource absorbed

--- lib/src/compatibility/day_master_compatibility.dart
+++ lib/src/compatibility/day_master_compatibility.dart
@@ -3,8 +3,10 @@ import '../elements.dart';
 import '../energies.dart';
 
 /// dummy non-existent unicorns for algorithm purpose
-const _perfectWife = AnimalSynergy(animal: Animal.Horse, flavor: Energy.EarthYin);
-const _perfectHusband = AnimalSynergy(animal: Animal.Dog, flavor: Energy.FireYin);
+const _perfectWife =
+    AnimalSynergy(animal: Animal.Horse, flavor: Energy.EarthYin);
+const _perfectHusband =
+    AnimalSynergy(animal: Animal.Dog, flavor: Energy.FireYin);
 
 List<AnimalSynergy> filteredWifeFor(Element day) {
   return wifeFor(day).where((a) => !isBadWife(a)).toList();
@@ -115,7 +117,8 @@ bool isBadWife(AnimalSynergy day) {
     (a) => a.animal == day.animal && a.flavor == day.flavor,
     orElse: () => _perfectWife,
   );
-  if (contained.animal != _perfectWife.animal && contained.flavor != _perfectWife.flavor) {
+  if (contained.animal != _perfectWife.animal &&
+      contained.flavor != _perfectWife.flavor) {
     return true;
   }
   return false;
@@ -130,7 +133,8 @@ bool isBadHusband(AnimalSynergy day) {
     (a) => a.animal == day.animal && a.flavor == day.flavor,
     orElse: () => _perfectHusband,
   );
-  if (contained.animal != _perfectHusband.animal && contained.flavor != _perfectHusband.flavor) {
+  if (contained.animal != _perfectHusband.animal &&
+      contained.flavor != _perfectHusband.flavor) {
     return true;
   }
   return false;
@@ -142,7 +146,8 @@ bool isJustBad(List<AnimalSynergy> sEt, AnimalSynergy person) {
     (a) => a.animal == person.animal && a.flavor == person.flavor,
     orElse: () => _perfectWife,
   );
-  if (contained.animal != _perfectWife.animal && contained.flavor != _perfectWife.flavor) {
+  if (contained.animal != _perfectWife.animal &&
+      contained.flavor != _perfectWife.flavor) {
     return true;
   }
   return false;
@@ -182,16 +187,20 @@ const bothGendersAbuser = <AnimalSynergy>[
   AnimalSynergy(animal: Animal.Goat, flavor: Energy.WaterYin),
 ];
 
-const femaleWealthAbsorbing = <AnimalSynergy>[
+const resourceAbsorbing = <AnimalSynergy>[
   AnimalSynergy(animal: Animal.Horse, flavor: Energy.WoodYang),
   AnimalSynergy(animal: Animal.Snake, flavor: Energy.WoodYin),
   AnimalSynergy(animal: Animal.Rat, flavor: Energy.MetalYang),
   AnimalSynergy(animal: Animal.Pig, flavor: Energy.MetalYin),
 ];
 
-const femaleWealthAbsorbed = <AnimalSynergy>[
+const resourceAbsorbed = <AnimalSynergy>[
   AnimalSynergy(animal: Animal.Dragon, flavor: Energy.FireYang),
   AnimalSynergy(animal: Animal.Dog, flavor: Energy.FireYang),
+  AnimalSynergy(animal: Animal.Goat, flavor: Energy.FireYin),
+  AnimalSynergy(animal: Animal.Ox, flavor: Energy.FireYin),
   AnimalSynergy(animal: Animal.Monkey, flavor: Energy.EarthYang),
+  AnimalSynergy(animal: Animal.Rooster, flavor: Energy.EarthYin),
   AnimalSynergy(animal: Animal.Tiger, flavor: Energy.WaterYang),
+  AnimalSynergy(animal: Animal.Rabbit, flavor: Energy.WaterYin),
 ];
