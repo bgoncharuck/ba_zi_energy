10a3de56ac43f6ee863a80f0ebd34b721a74e243
Bohdan Honcharuk <dariydragon26@gmail.com>
2024-07-17 20:27:50 +0300

    Update ba_zi_energy.dart

--- lib/ba_zi_energy.dart
+++ lib/ba_zi_energy.dart
@@ -6,5 +6,6 @@ export 'src/energies.dart';
 export 'src/animals.dart';
 export 'src/compatibility/partner_type.dart';
 export 'src/compatibility/zoo_compatibility.dart';
+export 'src/compatibility/element_compatibility.dart';
 export 'src/cache/month_start.dart';
 export 'src/cache/year_starts.dart';
