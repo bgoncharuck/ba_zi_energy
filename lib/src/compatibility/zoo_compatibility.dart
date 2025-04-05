6d04ce3e132b8e7f17102b59954f5f5591f0743a
Bohdan Honcharuk <dariydragon26@gmail.com>
2025-04-05 11:22:56 +0300

    feat: swap coins and swords

--- lib/src/compatibility/zoo_compatibility.dart
+++ lib/src/compatibility/zoo_compatibility.dart
@@ -1,8 +1,8 @@
 import '../animals.dart';
 
 Set<Animal> get wands => {Animal.Pig, Animal.Rabbit, Animal.Goat};
-Set<Animal> get coins => {Animal.Dragon, Animal.Monkey, Animal.Rat};
-Set<Animal> get swords => {Animal.Snake, Animal.Rooster, Animal.Ox};
+Set<Animal> get swords => {Animal.Dragon, Animal.Monkey, Animal.Rat};
+Set<Animal> get coins => {Animal.Snake, Animal.Rooster, Animal.Ox};
 Set<Animal> get cups => {Animal.Horse, Animal.Dog, Animal.Tiger};
 
 Set<Animal> get spring => {Animal.Tiger, Animal.Rabbit, Animal.Dragon};
